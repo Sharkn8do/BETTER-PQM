cac7d9a9d8c4c39cdf5b6235e256e5426dfe4f36
Sharkn8do <npc37@nau.edu>
2017-09-29 08:34:38 +0000

    add dbconnect, phone number validation, and beginning on the form

--- form.php
+++ form.php
@@ -1,3 +1,26 @@
-hello
-fuck off
-what
+<?php
+require_once "dbconnect.php";
+
+$query = "SELECT * FROM prints";
+echo $query;
+
+$result = $conn->query($query);
+
+while($row = $result->fetch_assoc()) {
+         echo $row['first_name'];
+     }
+
+
+?>
+<!DOCTYPE html>
+<head>
+  <title>Submit a 3D Print Request</title>
+</head>
+<body>
+  <form>
+    Your Name
+    </br>
+  <input type="text" width="100px">
+  
+  </form>
+</body>
---
+++ phonenumber.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+  /***phone number format***/
+  $(".phone").keypress(function (e) {
+    if (e.which != 8 && e.which != 0 && (e.which < 48 || e.which > 57)) {
+      return false;
+    }
+    var curchr = this.value.length;
+    var curval = $(this).val();
+    if (curchr == 3 && curval.indexOf("(") <= -1) {
+      $(this).val("(" + curval + ")" + " ");
+    } else if (curchr == 4 && curval.indexOf("(") > -1) {
+      $(this).val(curval + ")-");
+    } else if (curchr == 5 && curval.indexOf(")") > -1) {
+      $(this).val(curval + "-");
+    } else if (curchr == 9) {
+      $(this).val(curval + "-");
+      $(this).attr('maxlength', '14');
+    }
+  });
+});
