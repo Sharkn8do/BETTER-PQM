ea0d0d30f9794c482ecd48618c0319720d58badf
Sharkn8do <npc37@nau.edu>
2017-09-29 08:59:46 +0000

    added text boxed to form.php

--- form.php
+++ form.php
@@ -15,12 +15,24 @@ while($row = $result->fetch_assoc()) {
 <!DOCTYPE html>
 <head>
   <title>Submit a 3D Print Request</title>
+  <script type="text/javascript" src="phonenumber.js"></script>
+  <script type="text/javascript" src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
 </head>
 <body>
   <form>
     Your Name
     </br>
   <input type="text" width="100px">
+  </br>
+  Email Address
+  </br>
+  <input type="text" width="100px">
+</br>
+  Phone Number
+</br>
+<input id="phone" width="100px">
+
+  
   
   </form>
 </body>
