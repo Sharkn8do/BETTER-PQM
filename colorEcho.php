2231198b82932210d18b32705fa28cf503747f64
Sharkn8do <npc37@nau.edu>
2017-10-02 18:47:55 +0000

    Changed JSON encoding

--- colorEcho.php
+++ colorEcho.php
@@ -1,11 +1,9 @@
 <?php
 require_once "functions/dbconnect.php";
 					$query = "SELECT ColorDescription,ColorID FROM Colors ORDER BY ColorGroup";
-						echo $query;
 						$result = $conn->query($query);
 						while($row = $result->fetch_assoc()) {
 										 $colorData[] = $row;
 								 }
 echo json_encode($colorData);
-              
 					?>
