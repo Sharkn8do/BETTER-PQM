19369b2e811a136ccf997cba246be6cc9ec5fcc6
Sharkn8do <npc37@nau.edu>
2017-10-03 04:28:45 +0000

    Added Quotation Marks to  variables to stop the onslaught onto the log

--- colorEcho.php
+++ colorEcho.php
@@ -3,6 +3,6 @@ require_once "functions/dbconnect.php";
 					$query = "SELECT ColorDescription,ColorID FROM Colors ORDER BY ColorGroup";
 						$result = $conn->query($query);
 						while($row = $result->fetch_assoc()) {
-										 echo "<option id='" . $row[ColorID] . "'>" . $row[ColorDescription] . "</option>";
+										 echo "<option id='" . $row['ColorID'] . "'>" . $row['ColorDescription'] . "</option>";
 								 }
 					?>
